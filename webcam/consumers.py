ac3f0c9e842325bbeef36f493522dd6a1c6ddf4a
Quinos2003 <asthana.samarth2@gmail.com>
2023-05-10 02:33:11 +0530

    Modifying connect and disconnect feature

--- webcam/consumers.py
+++ webcam/consumers.py
@@ -2,12 +2,33 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class WebcamConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.room_name = "webcam_room"
+        self.room_group_name = f"webcam_group_{self.room_name}"
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    async def receive(self, text_data):
-        # Process received video frames (optional)
-        # Send processed video frames back to frontend
-        await self.send(text_data)
+
+# class WebcamConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         await self.accept()
+
+#     async def disconnect(self, close_code):
+#         pass
+
+#     async def receive(self, text_data):
+#         # Process received video frames (optional)
+#         # Send processed video frames back to frontend
+#         await self.send(text_data)
